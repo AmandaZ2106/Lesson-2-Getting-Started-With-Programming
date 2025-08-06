982d026cc8ae68db774957c60e6017f7a6efe669
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-06 19:31:04 +0100

    Project: Birthdays

---
+++ Project.py
@@ -0,0 +1,8 @@
+Amanda=21.6
+Ayden=15.8
+Mom=31.1
+Dad=10.5
+print("My birthday is on",Amanda,"\n")
+print("My brother's birthday is on",Ayden,"\n")
+print("My mother's birthday is on",Mom,"\n")
+print("My father's birthday is on",Dad,"\n")
